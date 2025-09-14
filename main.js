5205f983c79eb42e88f5749d999107a06141a9e5
adjieraka006-ship-it <adjieraka006@gmail.com>
2025-09-14 15:14:17 +0700

    update file latihan

---
+++ main.js
@@ -0,0 +1,32 @@
+// alert('heyho')
+
+// let usia = 30
+// usia = 35
+// console.log('heyhoo usia kamu adalah' + usia)
+// alert('usia kamu adalah' + usia)
+
+// let usia = prompt('berapa usia kamu?')
+// alert('usia anda adalah' + usia)
+
+
+let nama = 'ajie raka' // tipe string
+let usia = 25 //tipe integer number
+let tinggiBadan = 169.5 //tipe double float
+let beratBadan
+let pacar = null
+
+if (pacar == null) {
+    pacar = 'belum punya'
+} else {
+    pacar = 'udah punya'
+}
+
+let saldoAwal = 50000
+let saldoTambahan = 80000
+const saldoAkhir = saldoAwal + saldoTambahan
+
+alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah 
+    ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
+
+alert(`saldo awal saya sebesar ${saldoAwal} & saldo tambahan yang akan saya miliki 
+    sebesar ${saldoTambahan} jadi total saldo yang saya miliki adalah sebanyak  ${saldoAkhir}`)
